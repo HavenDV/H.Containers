1138992eb99568ec0479c899cd9e3c35ec43771f
Konstantin S <havendv@gmail.com>
2020-01-13 14:18:44 +1000

    Added template containers

---
+++ H.Containers.AssemblyLoadContext/AssemblyLoadContextContainer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace H.Containers.AssemblyLoadContext
+{
+    public class AssemblyLoadContextContainer : IDisposable
+    {
+        #region Events
+
+        public event EventHandler<Exception>? ExceptionOccurred;
+
+        private void OnExceptionOccurred(Exception exception)
+        {
+            ExceptionOccurred?.Invoke(this, exception);
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task LoadAssemblyAsync(string path, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task CreateObjectAsync(string typeName, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+    }
+}
