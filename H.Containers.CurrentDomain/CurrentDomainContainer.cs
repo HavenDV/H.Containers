2e86cc710733d80c02ad5eade5ff2d4c8298bfff
Konstantin S <havendv@gmail.com>
2020-01-14 07:41:25 +1000

    Implemented CurrentDomainContainer

--- H.Containers.CurrentDomain/CurrentDomainContainer.cs
+++ H.Containers.CurrentDomain/CurrentDomainContainer.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace H.Containers
 {
-    public sealed class CurrentDomainContainer : IDisposable
+    public sealed class CurrentDomainContainer : IContainer
     {
+        #region Properties
+
+        public string Name { get; }
+
+        public Assembly? Assembly { get; set; }
+
+        #endregion
+
         #region Events
 
         public event EventHandler<Exception>? ExceptionOccurred;
@@ -13,6 +23,16 @@ namespace H.Containers
         private void OnExceptionOccurred(Exception exception)
         {
             ExceptionOccurred?.Invoke(this, exception);
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public CurrentDomainContainer(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Name = (string.IsNullOrWhiteSpace(name) ? null : "") ?? throw new ArgumentException("Name is empty", nameof(name));
         }
 
         #endregion
@@ -25,6 +45,23 @@ namespace H.Containers
         }
 
         public Task LoadAssemblyAsync(string path, CancellationToken cancellationToken = default)
+        {
+            Assembly = Assembly.LoadFrom(path);
+
+            return Task.CompletedTask;
+        }
+
+        public Task<Type[]> GetTypesAsync(CancellationToken cancellationToken = default)
+        {
+            Assembly = Assembly ?? throw new InvalidOperationException("Assembly is not loaded");
+
+            var types = Assembly.GetTypes()
+                .ToArray();
+
+            return Task.FromResult(types);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default)
         {
             return Task.CompletedTask;
         }
