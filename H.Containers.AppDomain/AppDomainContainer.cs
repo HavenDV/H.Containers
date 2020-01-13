e62598a0b555cf7553fec2242b32d9d20d33ffd0
Konstantin S <havendv@gmail.com>
2020-01-14 07:41:13 +1000

    Implemented AppDomainContainer

--- H.Containers.AppDomain/AppDomainContainer.cs
+++ H.Containers.AppDomain/AppDomainContainer.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace H.Containers
 {
-    public class AppDomainContainer : IDisposable
+    public class AppDomainContainer : IContainer
     {
+        #region Properties
+
+        public string Name { get; }
+
+        public AppDomain? AppDomain { get; set; }
+        public Assembly? Assembly { get; set; }
+
+        #endregion
+
         #region Events
 
         public event EventHandler<Exception>? ExceptionOccurred;
@@ -17,15 +29,53 @@ namespace H.Containers
 
         #endregion
 
+        #region Constructors
+
+        public AppDomainContainer(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Name = (string.IsNullOrWhiteSpace(name) ? null : "") ?? throw new ArgumentException("Name is empty", nameof(name));
+        }
+
+        #endregion
+
         #region Public methods
 
         public Task StartAsync(CancellationToken cancellationToken = default)
         {
+            AppDomain = AppDomain.CreateDomain(Name);
+
             return Task.CompletedTask;
         }
 
         public Task LoadAssemblyAsync(string path, CancellationToken cancellationToken = default)
         {
+            AppDomain = AppDomain ?? throw new InvalidOperationException("Container is not started");
+
+            var bytes = File.ReadAllBytes(path);
+
+            Assembly = AppDomain.Load(bytes);
+
+            return Task.CompletedTask;
+        }
+
+        public Task<Type[]> GetTypesAsync(CancellationToken cancellationToken = default)
+        {
+            Assembly = Assembly ?? throw new InvalidOperationException("Assembly is not loaded");
+
+            var types = Assembly.GetTypes()
+                .ToArray();
+
+            return Task.FromResult(types);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default)
+        {
+            AppDomain = AppDomain ?? throw new InvalidOperationException("Container is not started");
+
+            AppDomain.Unload(AppDomain);
+            AppDomain = null;
+
             return Task.CompletedTask;
         }
 
@@ -40,6 +90,12 @@ namespace H.Containers
 
         public void Dispose()
         {
+            if (AppDomain == null)
+            {
+                return;
+            }
+
+            StopAsync().Wait();
         }
 
         #endregion
