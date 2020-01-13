5aae240d6b84050c029a6477dc2c9ff4313ffc88
Konstantin S <havendv@gmail.com>
2020-01-14 07:21:13 +1000

    Added Core project

---
+++ H.Containers.Core/IContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace H.Containers
+{
+    public interface IContainer : IDisposable
+    {
+        #region Properties
+
+        string Name { get; }
+
+        #endregion
+
+        #region Events
+
+        //event EventHandler<Exception>? ExceptionOccurred;
+
+        #endregion
+
+        #region Public methods
+
+        Task StartAsync(CancellationToken cancellationToken = default);
+        Task LoadAssemblyAsync(string path, CancellationToken cancellationToken = default);
+        Task<Type[]> GetTypesAsync(CancellationToken cancellationToken = default);
+        Task StopAsync(CancellationToken cancellationToken = default);
+        Task CreateObjectAsync(string typeName, CancellationToken cancellationToken = default);
+
+        #endregion
+    }
+}
