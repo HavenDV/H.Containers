e9606d139394eb26c0763ac473ce26aa23d4b18e
Konstantin S <havendv@gmail.com>
2020-01-13 14:18:56 +1000

    Added tests

--- H.Container.Tests/Tests.cs
+++ H.Container.Tests/Tests.cs
@@ -1,5 +1,6 @@
-﻿using System.Threading.Tasks;
-using H.Pipes;
+﻿using System;
+using System.Threading.Tasks;
+using H.Containers.Process;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace H.Container.Tests
@@ -7,21 +8,29 @@ namespace H.Container.Tests
     [TestClass]
     public class DataTests
     {
-        #region Tests
-
         [TestMethod]
-        public async Task StartDisposeStart()
+        public async Task StartTest()
         {
+            var receivedException = (Exception?) null;
+
+            using var container = new ProcessContainer();
+            container.ExceptionOccurred += (sender, exception) => receivedException = exception;
+
+            await container.ClearAsync();
+
+            await container.StartAsync();
+
+            await container.LoadAssemblyAsync("test");
+
+            for (var i = 0; i < 1000; i++)
             {
-                await using var pipe = new PipeServer<string>("test");
-                await pipe.StartAsync();
-            }
-            {
-                await using var pipe = new PipeServer<string>("test");
-                await pipe.StartAsync();
+                await Task.Delay(TimeSpan.FromMilliseconds(1));
+
+                if (receivedException != null)
+                {
+                    Assert.Fail(receivedException.ToString());
+                }
             }
         }
-
-#endregion
     }
 }
