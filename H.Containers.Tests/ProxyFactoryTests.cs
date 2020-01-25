b6ca00db44a9ae9f985301af591c074229ef67f8
Konstantin S <havendv@gmail.com>
2020-01-25 23:38:04 +1000

    Added CommonClass tests

--- H.Containers.Tests/ProxyFactoryTests.cs
+++ H.Containers.Tests/ProxyFactoryTests.cs
@@ -21,6 +21,8 @@ namespace H.Containers.Tests
             var result = instance.Test1("hello");
             Console.WriteLine($"Result: {result}");
             instance.Test2();
+
+            Assert.AreEqual(0, result);
         }
 
         public interface IInterface
@@ -51,6 +53,48 @@ namespace H.Containers.Tests
 
             var result = instance.Test1("hello");
             Console.WriteLine($"Result: {result}");
+            instance.Test2();
+
+            Assert.AreEqual(3, result);
+        }
+
+        public class CommonClass
+        {
+            public int Test1(string test)
+            {
+                return 1;
+            }
+
+            public void Test2()
+            {
+            }
+        }
+
+        [TestMethod]
+        public void CommonClassTest()
+        {
+            ProxyFactory.MethodCalled += (sender, args) =>
+            {
+                Console.WriteLine($"MethodCalled: {args.MethodInfo}");
+
+                if (args.Arguments.Any())
+                {
+                    Console.WriteLine("Arguments:");
+                }
+                for (var i = 0; i < args.Arguments.Count; i++)
+                {
+                    Console.WriteLine($"{i}: \"{args.Arguments[i]}\"");
+                }
+
+                args.ReturnObject = args.MethodInfo.ReturnType == typeof(int) ? 3 : args.ReturnObject;
+            };
+            var instance = ProxyFactory.CreateInstance<CommonClass>();
+
+            //var result = instance.GetType().InvokeMember("Test1", BindingFlags.InvokeMethod, null, instance, new object[] {"hello"});
+            var result = instance.Test1("hello");
+            Console.WriteLine($"Result: {result}");
+
+            Assert.AreEqual(1, result);
             instance.Test2();
         }
     }
