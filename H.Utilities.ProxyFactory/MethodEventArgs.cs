120de29946e004f40345b777ae40fc246e01321e
Konstantin S <havendv@gmail.com>
2020-01-26 02:55:38 +1000

    Added H.Utilities.ProxyFactory project and tests

---
+++ H.Utilities.ProxyFactory/MethodEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace H.Utilities
+{
+    public class MethodEventArgs : EventArgs
+    {
+        public List<object?> Arguments { get; set; }
+        public MethodInfo MethodInfo { get; set; }
+        public ProxyFactory ProxyFactory { get; set; }
+
+        public object? ReturnObject { get; set; }
+        public bool IsCanceled { get; set; }
+
+        public MethodEventArgs(List<object?> arguments, MethodInfo methodInfo, ProxyFactory proxyFactory)
+        {
+            Arguments = arguments ?? throw new ArgumentNullException(nameof(arguments));
+            MethodInfo = methodInfo ?? throw new ArgumentNullException(nameof(methodInfo));
+            ProxyFactory = proxyFactory ?? throw new ArgumentNullException(nameof(proxyFactory));
+        }
+    }
+}
--- H.Utilities.ProxyFactory/ProxyFactory.cs
+++ H.Utilities.ProxyFactory/ProxyFactory.cs
@@ -6,7 +6,7 @@ using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-namespace H.Containers
+namespace H.Utilities
 {
     public class ProxyFactory : IDisposable
     {
