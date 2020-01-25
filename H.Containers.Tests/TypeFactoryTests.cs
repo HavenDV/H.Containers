7a49f00aee8f156f67f8ded0d24eb4f35a7de924
Konstantin S <havendv@gmail.com>
2020-01-25 22:01:54 +1000

    Added TypeFactoryTests

---
+++ H.Containers.Tests/TypeFactoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace H.Containers.Tests
+{
+    [TestClass]
+    public class TypeFactoryTests
+    {
+        public abstract class AbstractClass
+        {
+            public abstract int Test1(string test);
+            public abstract void Test2();
+        }
+
+        [TestMethod]
+        public void AbstractTest()
+        {
+            var instance = TypeFactory.CreateInstance<AbstractClass>();
+
+            var result = instance.Test1("hello");
+            Console.WriteLine($"Result: {result}");
+            instance.Test2();
+        }
+
+        public interface IInterface
+        {
+            int Test1(string test);
+            void Test2();
+        }
+
+        [TestMethod]
+        public void InterfaceTest()
+        {
+            var instance = TypeFactory.CreateInstance<IInterface>();
+
+            var result = instance.Test1("hello");
+            Console.WriteLine($"Result: {result}");
+            instance.Test2();
+        }
+    }
+}
