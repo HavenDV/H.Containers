aced0a23b7b4eaf5ffe1c2b41c586ce83fdf7de5
Konstantin S <havendv@gmail.com>
2020-01-26 02:59:19 +1000

    Refactored

---
+++ H.Utilities.ProxyFactory.Tests/DirectProxyFactoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace H.Utilities.Tests
+{
+    [TestClass]
+    public class DirectProxyFactoryTests
+    {
+        [TestMethod]
+        public void CommonClassWithInterfaceTest()
+        {
+            using var factory = new DirectProxyFactory();
+            factory.MethodCalled += (sender, args) =>
+            {
+                Console.WriteLine($"MethodCalled: {args.MethodInfo}");
+
+                if (args.Arguments.Any())
+                {
+                    Console.WriteLine("Arguments:");
+                }
+                for (var i = 0; i < args.Arguments.Count; i++)
+                {
+                    Console.WriteLine($"{i}: \"{args.Arguments[i]}\"");
+                }
+
+                switch (args.MethodInfo.Name)
+                {
+                    case "Test2":
+                        args.IsCanceled = true;
+                        break;
+                }
+            };
+            factory.MethodCompleted += (sender, args) =>
+            {
+                Console.WriteLine($"MethodCompleted: {args.MethodInfo}");
+
+                if (args.Arguments.Any())
+                {
+                    Console.WriteLine("Arguments:");
+                }
+                for (var i = 0; i < args.Arguments.Count; i++)
+                {
+                    Console.WriteLine($"{i}: \"{args.Arguments[i]}\"");
+                }
+            };
+            var instance = factory.CreateInstance<ProxyFactoryTests.IInterface>(new ProxyFactoryTests.CommonClass());
+
+            var result = instance.Test1("hello");
+            Console.WriteLine($"Result: {result}");
+
+            Assert.AreEqual(1, result);
+            instance.Test2();
+        }
+    }
+}
--- H.Utilities.ProxyFactory.Tests/ProxyFactoryTests.cs
+++ H.Utilities.ProxyFactory.Tests/ProxyFactoryTests.cs
@@ -101,51 +101,5 @@ namespace H.Utilities.Tests
             Assert.AreEqual(1, result);
             instance.Test2();
         }
-
-        [TestMethod]
-        public void DirectProxyTest()
-        {
-            using var factory = new DirectProxyFactory();
-            factory.MethodCalled += (sender, args) =>
-            {
-                Console.WriteLine($"MethodCalled: {args.MethodInfo}");
-
-                if (args.Arguments.Any())
-                {
-                    Console.WriteLine("Arguments:");
-                }
-                for (var i = 0; i < args.Arguments.Count; i++)
-                {
-                    Console.WriteLine($"{i}: \"{args.Arguments[i]}\"");
-                }
-
-                switch (args.MethodInfo.Name)
-                {
-                    case "Test2":
-                        args.IsCanceled = true;
-                        break;
-                }
-            };
-            factory.MethodCompleted += (sender, args) =>
-            {
-                Console.WriteLine($"MethodCompleted: {args.MethodInfo}");
-
-                if (args.Arguments.Any())
-                {
-                    Console.WriteLine("Arguments:");
-                }
-                for (var i = 0; i < args.Arguments.Count; i++)
-                {
-                    Console.WriteLine($"{i}: \"{args.Arguments[i]}\"");
-                }
-            };
-            var instance = factory.CreateInstance<IInterface>(new CommonClass());
-
-            var result = instance.Test1("hello");
-            Console.WriteLine($"Result: {result}");
-
-            Assert.AreEqual(1, result);
-            instance.Test2();
-        }
     }
 }
