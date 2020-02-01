3fa3daf80c4cf9823bfa16ba311951f318cbcb21
Konstantin S <havendv@gmail.com>
2020-02-02 05:04:55 +1000

    Deleted CancellationTokenSources from PipeConnection methods

--- H.Utilities.PipeProxyFactory/PipeConnection.cs
+++ H.Utilities.PipeProxyFactory/PipeConnection.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using H.Pipes;
 using H.Pipes.Extensions;
-using H.Utilities.Extensions;
 
 namespace H.Utilities
 {
@@ -96,9 +95,6 @@ namespace H.Utilities
         /// <returns></returns>
         public async Task SendMessageAsync(string message, CancellationToken cancellationToken = default)
         {
-            using var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-            cancellationToken.RegisterSource(tokenSource);
-
             InternalConnection = InternalConnection ?? throw new InvalidOperationException("InternalConnection is null");
 
             await InternalConnection.WriteAsync(message, cancellationToken).ConfigureAwait(false);
@@ -114,14 +110,11 @@ namespace H.Utilities
         /// <returns></returns>
         public async Task SendAsync<T>(string name, T value, CancellationToken cancellationToken = default)
         {
-            using var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-            cancellationToken.RegisterSource(tokenSource);
-
             await using var client = new SingleConnectionPipeClient<object?>(name);
 
-            await client.ConnectAsync(tokenSource.Token);
+            await client.ConnectAsync(cancellationToken);
 
-            await client.WriteAsync(value, tokenSource.Token);
+            await client.WriteAsync(value, cancellationToken);
         }
 
         /// <summary>
@@ -133,14 +126,11 @@ namespace H.Utilities
         /// <returns></returns>
         public async Task<T> ReceiveAsync<T>(string name, CancellationToken cancellationToken = default)
         {
-            using var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-            cancellationToken.RegisterSource(tokenSource);
-
             await using var server = new SingleConnectionPipeServer<T>(name);
 
             var args = await server.WaitMessageAsync(
                 async token => await server.StartAsync(cancellationToken: token),
-                tokenSource.Token);
+                cancellationToken);
 
             return args.Message;
         }
