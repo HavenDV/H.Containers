c07fb0262a5cbc33acfc413a5afed353b9e258ed
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-09 18:14:19 +1000

    Updated ResourcesUtilities.

--- src/libs/H.Containers.Process/Utilities/ResourcesUtilities.cs
+++ src/libs/H.Containers.Process/Utilities/ResourcesUtilities.cs
@@ -7,45 +7,76 @@ using System.Reflection;
 
 namespace H.Containers.Utilities
 {
-    internal static class ResourcesUtilities
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ResourcesUtilities
     {
         /// <summary>
-        /// <![CDATA[Version: 1.0.0.1]]>
+        /// Searches for a file among Embedded resources <br/>
+        /// Throws an <see cref="ArgumentException"/> if nothing is found or more than one match is found <br/>
+        /// <![CDATA[Version: 1.0.0.4]]> <br/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="assembly"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         /// <returns></returns>
         public static Stream ReadFileAsStream(string name, Assembly? assembly = null)
         {
+            name = name ?? throw new ArgumentNullException(nameof(name));
             assembly ??= Assembly.GetExecutingAssembly();
 
-            var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith(name));
-
-            return assembly.GetManifestResourceStream(resourceName) ?? throw new ArgumentException($"\"{name}\" is not found in embedded resources");
-        }
-
-        public static string[] GetResourcesNames(Assembly? assembly = null)
-        {
-            assembly ??= Assembly.GetExecutingAssembly();
-
-            return assembly.GetManifestResourceNames().ToArray();
+            try
+            {
+                return assembly.GetManifestResourceStream(
+                           assembly
+                               .GetManifestResourceNames()
+                               .Single(resourceName => resourceName.EndsWith(name, StringComparison.InvariantCultureIgnoreCase)))
+                       ?? throw new ArgumentException($"\"{name}\" is not found in embedded resources");
+            }
+            catch (InvalidOperationException exception)
+            {
+                throw new ArgumentException(
+                    "Not a single one was found or more than one resource with the given name was found. " +
+                    "Make sure there are no collisions and the required file has the attribute \"Embedded resource\"", 
+                    exception);
+            }
         }
 
         /// <summary>
-        /// <![CDATA[Version: 1.0.0.1]]>
-        /// <![CDATA[Dependency: ReadFileAsStream(string name, Assembly? assembly = null)]]>
+        /// Searches for a file among Embedded resources <br/>
+        /// Throws an <see cref="ArgumentException"/> if nothing is found or more than one match is found <br/>
+        /// <![CDATA[Version: 1.0.0.2]]> <br/>
+        /// <![CDATA[Dependency: ReadFileAsStream(string name, Assembly? assembly = null)]]> <br/>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="assembly"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
         /// <returns></returns>
         public static byte[] ReadFileAsBytes(string name, Assembly? assembly = null)
         {
+            name = name ?? throw new ArgumentNullException(nameof(name));
+
             using var stream = ReadFileAsStream(name, assembly);
             using var memoryStream = new MemoryStream();
 
             stream.CopyTo(memoryStream);
 
             return memoryStream.ToArray();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static string[] GetResourcesNames(Assembly? assembly = null)
+        {
+            assembly ??= Assembly.GetExecutingAssembly();
+
+            return assembly.GetManifestResourceNames().ToArray();
         }
     }
 }
