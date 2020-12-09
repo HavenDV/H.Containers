631f856b1c5d09d69ec640470e58ffc42f14aaa0
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-09 18:33:22 +1000

    Added H.IO project.

---
+++ src/libs/H.IO/TempDirectory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+namespace H.IO
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TempDirectory : IDisposable
+    {
+        #region Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Folder { get; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool DeleteOnDispose { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="deleteOnDispose"></param>
+        public TempDirectory(bool deleteOnDispose = true)
+        {
+            DeleteOnDispose = deleteOnDispose;
+            
+            var random = new Random();
+            Folder = Path.Combine(Path.GetTempPath(), "H.Temp", $"{random.Next()}");
+            
+            Directory.CreateDirectory(Folder);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Clear()
+        {
+            if (!Directory.Exists(Folder))
+            {
+                return;
+            }
+            
+            foreach (var path in Directory.EnumerateFiles(Folder, "*.*", SearchOption.AllDirectories))
+            {
+                File.Delete(path);
+            }
+
+            Directory.Delete(Folder, true);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            if (!DeleteOnDispose)
+            {
+                return;
+            }
+
+            Clear();
+        }
+        
+        #endregion
+    }
+}
